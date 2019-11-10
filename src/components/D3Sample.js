69e2ed4a654f01e34075ad214e7001fac0346d9e
Creative332 <aleksej.strassheim@gmail.com>
2019-11-10 16:54:21 +0100

    Revert "Added d3.js example"
    This reverts commit 7045992547564357b9a79b34ed25e44350b613d2.

--- src/components/D3Sample.js
+++ src/components/D3Sample.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import {hot} from 'react-hot-loader';
-import * as d3 from 'd3';
 
 //import { geoMercator, geoPath } from 'd3-geo'
 
 class D3Sample extends Component {
     render() {
-
-        return <div><div id="my_dataviz"></div><svg id="test" width="500" height="500"></svg></div>;
+        return <h2 id="d3sample_pagetitle">D3 Sample</h2>;
     }
     // render() {
     //     const projection = geoMercator()
@@ -22,47 +20,6 @@ class D3Sample extends Component {
     //         {countries}
     //         </svg>
     //  }
-	componentDidMount(){
-    const data = [
-      {date: new Date(2019, 11, 5, 8, 0), value: 102},
-      {date: new Date(2019, 11, 5, 8, 30), value: 115},
-      {date: new Date(2019, 11, 5, 9, 0), value: 152},
-      {date: new Date(2019, 11, 5, 9, 30), value: 132},
-      {date: new Date(2019, 11, 5, 10, 0), value: 92},
-    ]
-
-    //Selects first element of type
-    const svg = d3.select('svg');
-    const height = +svg.attr('height');
-    const width = +svg.attr('width');
-    //Appends element and returns it
-    const g = svg.append('g').attr('transform', `translate(30,0)`);
-
-    const yScale = d3.scaleLinear().domain([400, 0]).range([0, height]);
-    const xScale = d3.scaleTime().domain(d3.extent(data, (d) => d.date)).range([0, width]);
-
-    const yAxis = d3.axisLeft().scale(yScale)
-    const xAxis = d3.axisBottom().scale(xScale)
-    const yAxisG = svg.append('g').attr('class', 'axisY').attr('transform', `translate(30,0)`)
-    const xAxisG = svg.append('g').attr('class', 'axisX').attr('transform', `translate(30,${height-20})`)
-    yAxisG.call(yAxis)
-    xAxisG.call(xAxis)
-
-
-    const test = function(d){
-      console.log(xScale(d))
-    };
-    var circs = g.selectAll('circle').data(data).join(
-      (enter) => enter.append('circle')
-                      .attr('cx', (d) => xScale(d.date))
-                      .attr('cy', (d) => yScale(d.value))
-                      .attr('r', 10)
-                      .attr('stroke', 'black'),
-      (update) => update,
-      (exit) => exit.remove()
-    );
-
-	}
 }
 
 export default hot ? hot(module)(D3Sample) : D3Sample;
