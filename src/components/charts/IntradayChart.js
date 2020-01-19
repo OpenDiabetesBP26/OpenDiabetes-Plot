8452aad6050418d2a9e9112a274255b386b61044
Xie, Yichen <yichen.xie@stud.tu-darmstadt.de>
2020-01-19 19:32:07 +0100

    axis, background,color von circle und legende added

--- src/components/charts/IntradayChart.js
+++ src/components/charts/IntradayChart.js
@@ -15,7 +15,8 @@ class IntradayChart extends Component {
     }
     componentDidMount() {
         if (this.props.data != null) {
-            let xAxis = d3.axisBottom(this.props.x);
+            let xAxis = d3.axisBottom(this.props.x)
+
             let yAxis = d3.axisLeft(this.props.y);
             let y = this.props.y;
             let margin = this.props.margin;
@@ -31,6 +32,8 @@ class IntradayChart extends Component {
                 .attr("height", y(185) - y(400))
                 .attr("width", width)
                 .attr("fill", "#f5f0b8")
+                .attr("transform", "translate(" + 0 + "," + 60 + ")")
+
             this.gcNormal = comp.append("rect")
                 .attr("class", "gcNormal")
                 .attr("y", y(180) + margin.top)
@@ -38,6 +41,8 @@ class IntradayChart extends Component {
                 .attr("height", y(70) - y(180))
                 .attr("width", width)
                 .attr("fill", "#e0e0e0")
+                .attr("transform", "translate(" + 0 + "," + 60 + ")")
+
             this.gcLow = comp.append("rect")
                 .attr("class", "gcLow")
                 .attr("y", y(65) + margin.top)
@@ -45,22 +50,59 @@ class IntradayChart extends Component {
                 .attr("height", y(0) - y(65))
                 .attr("width", width)
                 .attr("fill", "#faafaa")
+                .attr("transform", "translate(" + 0 + "," + 60 + ")")
 
-            this.circs = comp.append('g');
+
+
 
             //svg graph für background
             this.background = comp.append("g")
+            //add background für bar am top
+            this.topbar = comp.append("rect")
+                .attr("class", "topbar")
+                .attr("x", margin.left)
+                .attr("y", margin.top)
+                .attr("height", 25)
+                .attr("width", this.props.x.range()[1])
+                .attr("fill", "#2E86C1")
+                .style("opacity", 0.6)
+            //Chart Legende
+            this.legendeBackground = comp.append("rect")
+                .attr("class", "legendeBackground")
+                .attr("x", margin.left)
+                .attr("y", margin.top + 26)
+                .attr("height", 33)
+                .attr("width", this.props.x.range()[1])
+                .style("opacity", 0.3)
+                .style("fill", "#F8F9F9") //"#F8F9F9"
+
+            
+            this.legendeText = comp.append("text")
+                .text("BLOOD BLUCOSE     mg/dL")
+                .attr("class", "legendeText")
+                .attr("x", margin.left + 5)
+                .attr("y", margin.top + 47)
+                .style("font", "sans-serif")
+                .attr("fill", "black")
 
             //svg graph für x und y achse
             //init x und y achse
             this.xAxis_graph = comp.append("g")
                 .attr("class", "xline")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .style("color", "white")
+                .attr("transform", "translate(" + margin.left + "," + (margin.top + 25) + ")")
                 .call(xAxis);
+
+
+
             this.yAxis_graph = comp.append("g")
                 .attr("class", "yline")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .attr("transform", "translate(" + margin.left + "," + (margin.top + 60)+ ")")
                 .call(yAxis);
+
+            this.circs = comp.append('g');
+
+
 
             this.drawChart(this.props);
 
@@ -75,7 +117,7 @@ class IntradayChart extends Component {
         //wenn xAxis neue Scale bekommt, erneue Graph von xAxis und draw Background
         if (this.xAxis_graph) {
             //neue xAxis Daten von props
-            var newxAxis = d3.axisBottom(x)
+            var newxAxis = d3.axisTop(x)
             this.xAxis_graph.call(newxAxis)
             var bg = new BackGround();
             var readTicks = bg.readTicks(x);
@@ -96,23 +138,34 @@ class IntradayChart extends Component {
                     .attr('width', (d, i) => wdArr[i])
                     .style("opacity", (d, i) => opacityArr[i])
                 )
-                .attr('transform', 'translate(' + this.props.margin.left + ' ' + this.props.margin.top + ')');
+                .attr('transform', 'translate(' + this.props.margin.left + ' ' + (this.props.margin.top +60) + ')');
 
         }
 
-
+        //circle_color 
+        var circleColor = function(d) {
+            return d.value >= 185 ? '#3498DB' : d.value >= 65 ? '#58D68D' : '#DC7633';
+        }
 
         if (this.circs != null) {
             var circles = this.circs.selectAll('circle').data(props.data.glucose).join(
                     (enter) => enter.append('circle')
                     .attr('r', 3)
                     .attr('cy', d => y(+d.value))
-                    .attr('cx', d => x(d.time)),
+                    .attr('cx', d => x(d.time))
+                    .attr('fill', circleColor),
                     (update) => update
                     .attr('cy', d => y(+d.value))
                     .attr('cx', d => x(d.time))
+                    .attr('fill', circleColor)
                 )
-                .attr('transform', 'translate(' + this.props.margin.left + ' ' + this.props.margin.top + ')');
+                .attr('transform', 'translate(' + this.props.margin.left + ' ' + (this.props.margin.top + 60) + ')');
+        }
+
+        //update topbar
+        if (this.topbar) {
+            this.topbar.attr("width", this.props.x.range()[1]);
+            this.legendeBackground.attr("width", this.props.x.range()[1]);
         }
         //Update Normals for now
         if (this.gcHigh) {
@@ -126,6 +179,13 @@ class IntradayChart extends Component {
         }
 
     }
+    /*
+    creatColor(data){
+        var color = data.value >= this.props.y(185) ? '#3498DB' : data.value >= this.props.y(70) ? '##27AE60' : data.value >= this.props.y(0) ? '#DC7633' : '#FDFEFE'
+        return color;
+    }
+    */
+
 
     componentWillReceiveProps(nextProps) {
         //Wir können Daten hier neu rendern
--- src/services/BackGround.js
+++ src/services/BackGround.js
@@ -15,41 +15,29 @@ zurück
 class BackGround {
     constructor() {
         this.xLen = -1;
-        this.rectH = 370;//reguläre Hight von rects, fertig!
-        this.rectWs = [];//widths von rects für output, fertig!
+        this.rectH = 370; //reguläre Hight von rects, fertig!
+        this.rectWs = []; //widths von rects für output, fertig!
         this.xPos = []; //xposition von ticks für output, fertig!
         this.tickArray = []; //ticks für inere Verwendung, fertig!
         this.in6StufeOpacity = [0.9, 0.8, 0.7, 0.6, 0.5, 0.6] //6 stufen Opacity für innere Verwendung
-        this.in12StufeOpacity = [0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35]//12 stufen Opacity für innere Verwendung
-        this.in7tufeOpacity = [0.9, 0.8, 0.7, 0.6, 0.5, 0.6, 0.5]//7 stufen Opacity für innere Verwendung
-        this.in15StufeOpacity = [0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2]//15 stufen Opacity für innere Verwendung
+        this.in12StufeOpacity = [0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35] //12 stufen Opacity für innere Verwendung
+        this.in7tufeOpacity = [0.9, 0.8, 0.7, 0.6, 0.5, 0.6, 0.5] //7 stufen Opacity für innere Verwendung
+        this.in15StufeOpacity = [0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2] //15 stufen Opacity für innere Verwendung
     }
     //füge ein ein date-pading ein, die Zeitabstand stimmt mit der von folgenden Ticks überein.
     //读取ticks的日期，并且在index0位置加入一个与后续等距的date
     readTicks(d) {
         this.xLen = d.range()[1];
-        this.tickArray = d.ticks();
-        var firstDate = d.ticks()[0] - (d.ticks()[1] - d.ticks()[0]);
-        var oldfirst = this.tickArray[0]
-        var oldsecond = this.tickArray[1]
-        //var y = oldfirst.getFullYear() - (oldsecond.getFullYear() - oldfirst.getFullYear())
-        //var m = (oldfirst.getMonth() - (oldsecond.getMonth() - oldfirst.getMonth()) + 12) % 12
-        // var d = (oldfirst.getMonth() - (oldsecond.getMonth() - oldfirst.getMonth()) + 31) % 31
-        //var h = (oldfirst.getHours() - (oldsecond.getHours() - oldfirst.getHours()) + 24) % 24
-        // var ms = (oldfirst.getMinutes() - (oldsecond.getMinutes() - oldfirst.getMinutes()) + 60) % 60
-        var firstDate = new Date(oldfirst.getTime() - (oldsecond.getTime() - oldfirst.getTime()) + 24 * 60 * 60 * 1000 + 60 * 60 * 1000)
-        this.tickArray.splice(0, 0, firstDate);
-        //this.tickArray.splice(0, 0, new Date());
-        //console.log(this.tickArray)
+        let diff = d.ticks()[1] - d.ticks()[0];
+        let firstDate = new Date(d.ticks()[0].getTime() - diff);
+        this.tickArray[0] = firstDate;
+        this.tickArray = this.tickArray.concat(d.ticks());
         return this.tickArray;
     }
     //erzeuge ein Array mit x-position von allen ticks
     //第一个位x(0),其他的为scale的绝对位置
     creatXpos(d) {
-        this.xPos.push(0)
-        for (var i = 1; i < this.tickArray.length; i++) {
-            this.xPos.push(d(this.tickArray[i]))
-        }
+        this.tickArray.map((e, i) => i == 0 ? this.xPos[i] = 0 : this.xPos[i] = d(e))
         //console.log("xPos", this.xPos)
         return this.xPos;
     }
@@ -66,8 +54,27 @@ class BackGround {
         if (this.tickArray.length <= 2) {
             return 'intraday';
         } else {
-            var tickStep = this.tickArray[2].getTime() - this.tickArray[1].getTime()
-            var result = tickStep >= 350 * 24 * 60 * 60 * 1000 ? 'yearly' : tickStep >= 4 * 7 * 24 * 60 * 60 * 1000 ? 'monthly' : tickStep >= 7 * 24 * 60 * 60 * 1000 ? 'weekly' : tickStep >= 12 * 60 * 60 * 1000 ? 'daily' : tickStep >= 60 * 60 * 1000 ? 'hourly' : tickStep >= 30 * 60 * 1000 ? 'halfhour' : tickStep >= 15 * 60 * 1000 ? 'quarter' : tickStep >= 5*60*1000 ? '5min' : 'min'
+            let tickStep = this.tickArray[2].getTime() - this.tickArray[1].getTime()
+            let result = '';
+            if (tickStep >= 350 * 24 * 60 * 60 * 1000) {
+                result = 'yearly'
+            } else if (tickStep >= 4 * 7 * 24 * 60 * 60 * 1000) {
+                result = 'monthly'
+            } else if (tickStep >= 7 * 24 * 60 * 60 * 1000) {
+                result = 'weekly'
+            } else if (tickStep >= 12 * 60 * 60 * 1000) {
+                result = 'daily'
+            } else if (tickStep >= 60 * 60 * 1000) {
+                result = 'hourly'
+            } else if (tickStep >= 30 * 60 * 1000) {
+                result = 'halfhour'
+            } else if (tickStep >= 15 * 60 * 1000) {
+                result = 'quarter'
+            } else if (tickStep >= 5 * 60 * 1000) {
+                '5min'
+            } else {
+                'min'
+            }
             return result;
         }
     }
@@ -92,7 +99,7 @@ class BackGround {
                 return this.creatQuarterOpacity()
             case '5min':
                 return this.creat5minOpacity()
-            case 'min' : 
+            case 'min':
                 return this.creatminOpacity()
 
         }
@@ -101,11 +108,11 @@ class BackGround {
 
 
     creatYearlyOpacity() {
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         //console.log("creatYearlyOpacity")
-        var OpacitySeed = this.in6tufeOpacity
-        var yearBase = this.tickArray[0].getFullYear()
+        OpacitySeed = this.in6tufeOpacity
+        let yearBase = this.tickArray[0].getFullYear()
         this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[d.getFullYear() - yearBase])
         return rectOpacity;
     }
@@ -113,17 +120,17 @@ class BackGround {
 
     //erzeuge monthlyOpacity
     creatMonthlyOpacity() {
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         //console.log("creatMonthlyOpacity")
-        var OpacitySeed = this.in6StufeOpacity
+        OpacitySeed = this.in6StufeOpacity
         this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[d.getMonth() % 6]);
         return rectOpacity;
     }
 
     creatWeeklyOpacity() {
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         //console.log("creatWeeklyOpacity")
         //星期数一样，那么就是一周一个刻度
         if (this.tickArray[1].getDay() == this.tickArray[2].getDay()) {
@@ -142,8 +149,8 @@ class BackGround {
 
 
     creatInDailyOpcaity() {
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         //console.log("creatInDailyOpcaity")
         OpacitySeed = this.in15StufeOpacity
         this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[d.getDate() % 15]);
@@ -154,8 +161,8 @@ class BackGround {
 
     creatHourlyOpacity() {
         //console.log("creatHourlyOpacity")
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         OpacitySeed = this.in12StufeOpacity
         this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[d.getHours() % 12]);
         return rectOpacity;
@@ -165,40 +172,40 @@ class BackGround {
 
     creatQuarterOpacity() {
         //console.log("creatQuarterOpacity")
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         OpacitySeed = this.in6StufeOpacity
-        this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[(d.getHours()*60 + d.getMinutes()) % 4]);
+        this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[(d.getHours() * 60 + d.getMinutes()) % 4]);
         return rectOpacity;
     }
     creathalfhourlyOpacity() {
         //console.log("creathalfhourlyOpacity")
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         OpacitySeed = this.in12StufeOpacity
-        this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[(d.getHours()*60 + d.getMinutes()) % 12]);
+        this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[(d.getHours() * 60 + d.getMinutes()) % 12]);
         return rectOpacity;
     }
 
-    creat5minOpacity(){
+    creat5minOpacity() {
         //console.log("creat5minOpacity")
-        var OpacitySeed = []
-        var rectOpacity = []
+        let OpacitySeed = []
+        let rectOpacity = []
         OpacitySeed = this.in12StufeOpacity
-        this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[(d.getHours()*60 + d.getMinutes()) % 12]);
+        this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[(d.getHours() * 60 + d.getMinutes()) % 12]);
         return rectOpacity;
     }
-    creatminOpacity(){
+    creatminOpacity() {
         //console.log("creatminOpacity")
-        var OpacitySeed = []
-        var alpha = 0.9
-        for(var i = 0; i < 30; i++){
+        let OpacitySeed = []
+        let alpha = 0.9
+        for (var i = 0; i < 30; i++) {
             OpacitySeed[i] = alpha
-            alpha -= (0.9/30)
+            alpha -= (0.9 / 30)
         }
-        var rectOpacity = []
+        let rectOpacity = []
         this.tickArray.forEach((d, i) => rectOpacity[i] = OpacitySeed[d.getMinutes() % 30]);
-        return rectOpacity;   
+        return rectOpacity;
     }
 
 }
