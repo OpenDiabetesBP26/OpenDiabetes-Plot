d75ee25e5fded5e891f80afa9d02c9cc9183f379
Aleksej <aleksej.strassheim@gmail.com>
2020-02-07 13:21:39 +0100

    restructred Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,13 +3,19 @@ import { hot } from 'react-hot-loader';
 import Loading from '../common/Loading';
 import * as d3 from 'd3';
 import DataManager from '../services/DataManager';
-import TimeAxis from './charts/TimeAxis';
+
 
 class Chart extends Component {
     constructor(props) {
         super(props);
-        this.state = { loading: true }
-        this.svg = React.createRef()
+        this.dataManager = new DataManager();
+        this.renderData = undefined;
+        this.x = d3.scaleTime();
+        this.xBase = d3.scaleTime();
+        this.state = { loading: false, x: this.x }
+        this.margin = { top: 20, right: 40, bottom: 110, left: 40 }
+
+        this.loadTestData = this.loadTestData.bind(this);
 
     }
     render() {
@@ -18,126 +24,102 @@ class Chart extends Component {
                 <Loading visible={this.state.loading} />
                 <div className="row">
                     <div className="col-lg-8 col-md-12">
+                        {!this.data && <div> No data loaded </div>}
                         <svg id="d3sample" width="100%" height="500" ref={(svg) => this.svg = svg}>
-                            <TimeAxis x={this.state.x} />
+                        
                         </svg>
                     </div>
                     <div className="col-lg-4 col-md-12">
-                       
+                    <button onClick={this.loadTestData}>
+                        Load test data
+                        </button>
                     </div>
                 </div>
             </div>
         );
     }
 
-    //Wird einmalig aufgerufen, wenn es gemountet ist
-    async componentDidMount() {
+
+    componentDidMount() {
+        //this.loadData();
+
+        //Append resize listener
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+        // //Call resize to set first state
+    }
+    componentWillUnmount(){
+        window.removeEventListener("resize", this.updateDimensions.bind(this));
+    }
+    async loadTestData(){
         try {
             //TODO Implement DataManager
             let data = await (await fetch("/data/2019-11-20-1349_export-data.json")).json()
             console.log(data);
             data = data.data;
-            this.data = data;
+            this.loadData(data);
 
         }
         finally {
-            let dm = new DataManager();
-            dm.readData(this.data);
-            this.dm = dm;
-            this.maxZoom = dm.getMaxZoom();
             this.setState({ loading: false });
 
-            //Add d3 stuff
-            let svg = d3.select("svg");
-            let width = 1000,
-                height = 400,
-                margin = { top: 20, right: 40, bottom: 110, left: 40 }
-            this.margin = margin;
-            //Domains
-            let xBase = d3.scaleTime().range([0, width]),
-                x = d3.scaleTime().range([0, width]),
-                y = d3.scaleLinear().range([height, 0]);
-
-            //Set max domains
-            x.domain(this.dm.getMaxDomain());
-            y.domain([0, 400])
-            xBase.domain(x.domain());
-
-            //TODO set base domain through data manager
-
-            //Update State with domains
-            this.setState({ xBase: xBase, x: x, y: y})
-
-
-            this.zoom = d3.zoom()
-                //Wie viel man unzoomen und zoomen kann
-                //TODO vom DataManager berechnen lassen
-                .scaleExtent([1, this.maxZoom])
-                .translateExtent([[0, 0], [width, 0]])
-                .on("zoom", () => this.zoomed());
-
-            svg.call(this.zoom);
-            this.d3svg = svg;
-            
-
-            //Append resize listener
-            window.addEventListener("resize", this.updateDimensions.bind(this));
-            //Call resize to set first state
-            this.updateDimensions();
         }
+        
+
+    }
+    loadData(data) {
+        this.data = data;
+        this.dataManager.readData(data);
+        this.maxZoom = this.dataManager.getMaxZoom();
+        //Add d3 stuff
+        let svg = d3.select(this.svg);
+
+        //Creating new domains
+        this.x.domain(this.dataManager.getMaxDomain());
+        this.xBase.domain(this.x.domain());
+        //Creating new zoom
+        this.zoom = d3.zoom()
+            .scaleExtent([1, this.maxZoom])
+            .translateExtent([[0, 0], [0, 0]])
+            .on("zoom", () => this.zoomed());
+        //Calling zoom on svg
+        svg.call(this.zoom);
+        //updateDimensions to fix zoom
+        this.updateDimensions();
+
     }
     updateDimensions() {
-        if (this.svg) {
+        if (this.svg && this.zoom) {
             let newWidth = this.svg.getBoundingClientRect().width - this.margin.left - this.margin.right;
             //Update Base and x range
-            let xBase = this.state.xBase.range([0, newWidth]);
-            let x = this.state.x.range([0, newWidth]);
+            let xBase = this.xBase.range([0, newWidth]);
+            let x = this.x.range([0, newWidth]);
 
             //Update Zoom
             //Fix, as d3.event.transform is null in webpack
-            let oldRange = this.state.xBase.range()[1] - this.state.xBase.range()[0];
-            let s = this.state.x.domain().map(x => xBase(x));
+            let oldRange = this.xBase.range()[1] - this.xBase.range()[0];
+            let s = this.x.domain().map(x => xBase(x));
             let k = oldRange / (s[1] - s[0]);
 
             let newXOffset = -xBase(x.domain()[0]);
-            console.log(this.zoom.translateExtent());
-            this.fixExtent();
-            d3.select("svg").call(this.zoom.transform, d3.zoomIdentity.scale(k).translate(newXOffset, 0));
+            //Get current svg size
+            let width = this.svg.getBoundingClientRect().width - this.margin.left - this.margin.right;
+            //Get currrent zoom
+            let kFix = (d3.event != null ? d3.event.transform.k : 1);
+            //From https://stackoverflow.com/questions/44120372/d3-v4-how-to-limit-left-right-panning-on-an-x-zoom-line-graph
+            this.zoom.translateExtent([[0, 0], [width + ((this.margin.left + this.margin.right) / kFix), 0]])
+            d3.select(this.svg).call(this.zoom.transform, d3.zoomIdentity.scale(k).translate(newXOffset, 0));
 
             //Update State
             this.setState({ xBase: xBase, x: x });
 
         }
     }
-    /** translateExtent locks panning after specific point.
-     *  However, it seems buggy while zooming and resizing
-     *  Code below needs to be applied to fix it
-     * 
-     */
-    fixExtent() {
-        //Get current svg size
-        let width = this.svg.getBoundingClientRect().width - this.margin.left - this.margin.right;
-        //Get currrent zoom
-        let k = (d3.event != null ? d3.event.transform.k : 1);
-        //From https://stackoverflow.com/questions/44120372/d3-v4-how-to-limit-left-right-panning-on-an-x-zoom-line-graph
-        this.zoom.translateExtent([[0, 0], [width + ((this.margin.left + this.margin.right) / k), 0]])
-    }
-
     zoomed() {
-        //this.fixExtent();
-
-        let x = d3.event.transform.rescaleX(this.state.xBase);
-        //Update Domain in sync
-        //Update Display
-
-        //Get new data
-        let data = this.dm.getRenderData(x.domain());
-        console.log(data);
-
-        
+        this.x = d3.event.transform.rescaleX(this.xBase);
+        this.renderData = this.dataManager.getRenderData(this.x.domain());
         //Set State
         this.setState({
-            x: x,
+            x: this.x,
         });
 
     }
