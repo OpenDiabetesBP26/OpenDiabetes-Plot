a50a29a717a1c2685ed53b802a276638d148f5c7
Konstantin Strassheim <konstantin.strassheim@googlemail.com>
2020-02-10 17:01:36 +0000

    Merged PR 68: 146-cleanup2-seperated-file-load-and-view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import Chart from './components/Chart';
+import Loading from './components/Loading';
 import {hot} from 'react-hot-loader';
 
 export class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { loading:true, data:null }
+        this.loadTestData();
+    }
+
     render() {
         return (
-            <Chart />
+            <React.Fragment>
+                <Loading visible={this.state.loading} />
+                <Chart data={this.state.data} />
+            </React.Fragment>
         );
+    }
+
+    async componentDidMount() {
+        await this.loadTestData();
+    }
+    
+    async loadTestData(){
+        try {
+            //TODO Implement DataManager
+            let data = await (await fetch("2019-11-20-1349_export-data.json")).json()
+            //console.log(data);
+            this.setState({data:data.data});
+        }
+        finally {
+            this.setState({ loading: false });
+        }
     }
 
 }
